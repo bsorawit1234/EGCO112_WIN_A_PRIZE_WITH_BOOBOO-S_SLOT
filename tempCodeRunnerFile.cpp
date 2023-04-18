29f287f750aa883fa11ee750b06d9143aa03a058
bsorawit1234 <bsorawit1234@gmail.com>
2023-04-18 22:34:53 +0700

    test

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-  random_num[0] = rand() % 10;
-      random_num[1] = rand() % 10;
-      random_num[2] = rand() % 10;
-
-      cout << "\r" << random_num[0] << random_num[1] << random_num[2]; // print the number with a carriage return to overwrite the previous number
-      
-      fflush(stdout); // flush the output buffer
-      usleep(100000); // add a delay of 100ms between each frame
-    }
