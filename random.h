71ea3643cb1ac68123fcfc0f7edd0bd051c74fc2
bsroawit1234 <sorawit.pha@student.mahidol.edu>
2023-04-20 16:41:06 +0700

    test

--- random.h
+++ random.h
@@ -12,3 +12,15 @@ int *random_slot() {
   }
   return random_num;
 }
+
+// // Rules
+// int *random = random_slot();
+// if(random[0] == random[1] && random[1] == random[2]) {
+//   money *= 10;
+// } else if(random[2] - random[1] == 1 && random[1] - random[0] == 1) {
+//   money *= 5;
+// } else if(random[2] - random[1] == -1 && random[1] - random[0] == -1) {
+//   money *= 5;
+// } else if(random[0] == random[2]) {
+//   money *= 3;
+// }
