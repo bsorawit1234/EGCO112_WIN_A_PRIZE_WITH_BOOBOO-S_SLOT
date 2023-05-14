a2fb046f73379117521777395a71d7fbb0b92ac0
bsorawit1234 <bsorawit1234@gmail.com>
2023-05-14 15:57:24 +0700

    add admin topup

--- Admin.h
+++ Admin.h
@@ -1,8 +1,8 @@
 // #include "User.h"
 
 void admin_page(ListUser A) {
-  int choice = 2;
-  std::string name = "boom";
+  int choice, check_choice = 1;
+  std::string name;
   User* temp;
   std::string for_read, for_write, target, pass, ranks, m, c, rank;
   std::ifstream read("Total.txt");
@@ -10,13 +10,64 @@ void admin_page(ListUser A) {
 
   system("@cls||clear");
   std::cout << "-------- ADMIN ---------" << std::endl;
-  std::cout << "  1. TOPUP" << std::endl;
-  std::cout << "  2. GIVE VIP" << std::endl;
-  std::cout << "-----------------------" << std::endl;
-  std::cout << "CHOICE: " << std::endl;
+  std::cout << "     1. GIVE MONEY" << std::endl;
+  std::cout << "     2. GIVE VIP" << std::endl;
+  std::cout << "------------------------" << std::endl;
+
+  while(check_choice) {
+    try {
+      std::cout << "CHOICE: ";
+      std::cin >> choice;
+
+      if(std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        throw "ONLY NUMBER !!";
+      }
+
+      if(choice != 1 && choice != 2) {
+        throw "CHOOSE BETWEEN 1 AND 2";
+      }
+      check_choice = 0;
+    } catch(const char* s) {
+      std::cout << std::endl << s << std::endl << std::endl;
+    }
+  }
+  
+  if(choice == 1) {
+    std::cout << "Who do you want to give money: ";
+    std::getline(std::cin, name);
+    
+    float m;
+    int check_m = 1;
+    
+    while(check_m) {
+      try {
+        std::cout << "How much money: ";
+        std::cin >> m;
+
+        if(std::cin.fail()) {
+          std::cin.clear();
+          std::cin.ignore(256,'\n');
+          throw "ONLY NUMBER !!";
+        }
+        if(m < 100) {
+          throw "MINIMUM MONEY IS $100";
+        }
+        check_choice = 0;
+      } catch(const char* s) {
+        std::cout << std::endl << s << std::endl << std::endl;
+      }
+    }
+
+    A.find_node(name)->topup(m);
+  }
 
   if(choice == 2) {
     //change class. 
+    std::cout << "Who do you want to give VIP: " << std::endl;
+    std::getline(std::cin, name);
+    
     temp = A.find_node(name);
     A.class_changes(temp);
 
